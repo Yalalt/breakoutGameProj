da34614813509ca3accd958497c99e8fe1eb0a54
Yalalt <yalaltgandush@gmail.com>
2023-06-12 12:35:44 +0800

    Created javadoc documentation

--- src/breakout/Breakout.java
+++ src/breakout/Breakout.java
@@ -1,21 +1,33 @@
 package breakout;
 
 import java.awt.Color;
+import java.awt.event.MouseEvent;
 
 import acm.graphics.*;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
+/**
+ * Breakout game main class
+ * @author yalalt
+ * @version 1.0.0
+ *
+ */
+@SuppressWarnings("serial")
 public class Breakout extends GraphicsProgram {
 
 	private GOval ball = new GOval(WIDTH / 2, HEIGHT / 2, BALL_RADIUS, BALL_RADIUS);
-	private double vx, vy;
+	private double vx = 0, vy;
 	private boolean isPlaying = true;
 	private GRect paddle = new GRect(PADDLE_WIDTH, PADDLE_HEIGHT);
 	private GRect[] toosgo = new GRect[NROW * NBLOCKS_PER_ROW];
 	private int removedBlocks = 0, ndie = 0, i;
 
+	/**
+	 * Main run function
+	 */
 	public void run() {
+		addMouseListeners();
 		setSize(WIDTH, HEIGHT);
 		initGame();
 		playGame();
@@ -27,24 +39,41 @@ public class Breakout extends GraphicsProgram {
 		GLabel title = new GLabel("Game over");
 		title.setColor(Color.RED);
 		title.setLocation(WIDTH/2, HEIGHT/2);
-		if(removeBlocks >= NROW * NBLOCKS_PER_ROW) {
+		if(removedBlocks >= NROW * NBLOCKS_PER_ROW) {
 			title.setLabel("You win");
 			title.setColor(Color.GREEN);
 		} else if(ndie >= NTURNS) title.setLabel("You lose");
 		
 		add(title);
 	}
+	
+	/**
+	 * Mouse move event
+	 * Move by mouse position
+	 */
+	public void mouseMoved(MouseEvent e) {
+		int x = e.getX();
+		if(x >= WIDTH - PADDLE_WIDTH) x = WIDTH - PADDLE_WIDTH;
+		
+		paddle.setLocation(x, paddle.getY());
+	}
 
+	/**
+	 * Game start init values
+	 */
 	public void initGame() {
 		initBlocks();
 		initPaddle();
 	}
 
+	/**
+	 * initialize ball
+	 */
 	public void initBall() {
 		RandomGenerator rgen = RandomGenerator.getInstance();
 		ball.setLocation(WIDTH / 2, HEIGHT / 2);
 		ball.setFilled(true);
-		vx = rgen.nextDouble(1.0, 2.0);
+		vx = vx + rgen.nextDouble(1.0, 2.0);
 		vy = 3;
 
 		if (rgen.nextBoolean(0.5))
@@ -54,11 +83,18 @@ public class Breakout extends GraphicsProgram {
 		add(ball);
 	}
 
+	/**
+	 * Paddle prepare function
+	 */
 	public void initPaddle() {
 		paddle.setFilled(true);
 		add(paddle, WIDTH / 2 - PADDLE_WIDTH / 2, PADDLE_Y_OFFSET);
 	}
-
+	
+	
+	/**
+	 * Blocks prepare function and initialize
+	 */
 	public void initBlocks() {
 		int dx;
 		double dy;
@@ -78,6 +114,9 @@ public class Breakout extends GraphicsProgram {
 		}
 	}
 
+	/**
+	 * Control blocks, ball, ball position method when game playing
+	 */
 	public void playGame() {
 		initBall();
 		while (isPlaying) {
@@ -90,6 +129,7 @@ public class Breakout extends GraphicsProgram {
 					remove(toosgo[i]);
 					removedBlocks++;
 					vy = -vy;
+					vx++;
 				}
 			}
 
@@ -109,7 +149,7 @@ public class Breakout extends GraphicsProgram {
 			if (ball.getLocation().getY() <= 0)
 				vy = -vy;
 
-			if (ball.getLocation().getX() <= 0 || ball.getLocation().getX() >= WIDTH)
+			if (ball.getLocation().getX() <= 0 || ball.getLocation().getX() + BALL_RADIUS*2 >= WIDTH)
 				vx = -vx;
 			
 			pause(20);
@@ -119,10 +159,17 @@ public class Breakout extends GraphicsProgram {
 		}
 	}
 
+	/**
+	 * Ball move method
+	 */
 	public void ballMove() {
 		ball.move(vx, vy);
 	}
 
+	/**
+	 * Find object when ball collide with bricks
+	 * @return GObject
+	 */
 	public GObject getCollidingObject() {
 		GObject result = null;
 
@@ -132,36 +179,36 @@ public class Breakout extends GraphicsProgram {
 		return result;
 	}
 
-	// Game board of width, height
+	/** Game board of width, height */
 	private static final int WIDTH = 400;
 	private static final int HEIGHT = 600;
 
-	// Paddle width, height
+	/** Paddle width, height */
 	private static final int PADDLE_WIDTH = 60;
 	private static final int PADDLE_HEIGHT = 10;
 
-	// Paddle on bottom Y
+	/** Paddle on bottom Y */
 	private static final int PADDLE_Y_OFFSET = 500;
 
-	// Number block per row
+	/** Number block per row */
 	private static final int NBLOCKS_PER_ROW = 5;
 
-	// Number rows per game start
+	/** Number rows per game start */
 	private static final int NROW = 5;
 
-	// Blocks between gap size
+	/** Blocks between gap size */
 	private static final int BLOCKS_GAP = 2;
 
-	// Block width, height
+	/** Block width, height */
 	private static final int BLOCK_WIDTH = (WIDTH - (NBLOCKS_PER_ROW - 1) * BLOCKS_GAP) / NBLOCKS_PER_ROW;
 	private static final int BLOCK_HEIGHT = 20;
 
-	// Ball radius
+	/** Ball radius */
 	private static final int BALL_RADIUS = 8;
 
-	// Top row position offset coord
+	/** Top row position offset coordinate */
 	private static final int BLOCK_Y_OFFSET = 50;
 
-	// Total live number
+	/** Total live number */
 	private static final int NTURNS = 3;
 }
